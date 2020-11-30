921ebfb68a229aa7d496b51bba3e6d7e880a8d5a
Luis Yauri <david.yauri147@gmail.com>
2020-11-29 19:16:33 -0500

    first commit

---
+++ src/app/ng-zorro/ng-zorro.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core'
+import { NzButtonModule } from 'ng-zorro-antd/button'
+import { NzModalModule } from 'ng-zorro-antd/modal'
+import { NzTableModule } from 'ng-zorro-antd/table'
+import { NzIconModule } from 'ng-zorro-antd/icon'
+import { NzFormModule } from 'ng-zorro-antd/form'
+import { NzInputModule } from 'ng-zorro-antd/input'
+import { NzTabsModule } from 'ng-zorro-antd/tabs'
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker'
+import { NzSelectModule } from 'ng-zorro-antd/select'
+import { NzUploadModule } from 'ng-zorro-antd/upload'
+import { NzNotificationModule } from 'ng-zorro-antd/notification'
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown'
+import { NzSwitchModule } from 'ng-zorro-antd/switch'
+import { NzMenuModule } from 'ng-zorro-antd/menu'
+import { NzCollapseModule } from 'ng-zorro-antd/collapse'
+import { NzGridModule } from 'ng-zorro-antd/grid'
+import { NzRadioModule } from 'ng-zorro-antd/radio'
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox'
+import { NzMessageModule } from 'ng-zorro-antd'
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm'
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number'
+
+@NgModule({
+  exports: [
+    NzButtonModule,
+    NzModalModule,
+    NzTableModule,
+    NzTableModule,
+    NzIconModule,
+    NzFormModule,
+    NzInputModule,
+    NzTabsModule,
+    NzDatePickerModule,
+    NzSelectModule,
+    NzUploadModule,
+    NzNotificationModule,
+    NzDropDownModule,
+    NzSwitchModule,
+    NzMenuModule,
+    NzCollapseModule,
+    NzGridModule,
+    NzRadioModule,
+    NzCheckboxModule,
+    NzMessageModule,
+    NzPopconfirmModule,
+    NzInputNumberModule,
+  ],
+})
+export class NgZorroModule {}
